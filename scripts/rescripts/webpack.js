1a180572b182d7606f9079c4428af374b6f21712
SarahNaik1 <sarahnaik2@gmail.com>
2022-02-05 14:04:31 +0530

    Added webpack plugin

--- scripts/rescripts/webpack.js
+++ scripts/rescripts/webpack.js
@@ -1,6 +1,16 @@
 const { removeWebpackPlugin } = require('@rescripts/utilities')
+const webpack = require('webpack')
+const moduleReplacementCallback = require('../resourceFinder')
+
+const resourcesRegex = /\$RESOURCES/
+const suffixes = ['', '.js', '.tsx']
 
 module.exports = (config) => {
+  config.plugins = [
+    new webpack.NormalModuleReplacementPlugin(resourcesRegex, (resource) => {
+      moduleReplacementCallback(resource, suffixes)
+    }),
+  ]
   const webpackWithoutEsLint = removeWebpackPlugin('ESLintWebpackPlugin', config)
   return webpackWithoutEsLint
 }
