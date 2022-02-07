19b763427c0c84153160d960bf561410a9fd290b
PAM <das.pamela111@gmail.com>
2022-02-07 18:31:33 +0530

    Initial commit check

--- src/routes/welcome/Welcome.tsx
+++ src/routes/welcome/Welcome.tsx
@@ -13,7 +13,7 @@ function Welcome(): ReactElement {
     <Page align="center">
       <Block>
         <Title size="md" strong>
-          Welcome to Gnosis Safe.
+          Welcome to Gnosis Safe_HSBC.
         </Title>
         <Title size="xs">
           Gnosis Safe is the most trusted platform to manage digital assets. <br /> Here is how to get started:
